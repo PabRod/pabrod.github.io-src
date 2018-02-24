413ea818516a6fb25ed480048e6d88ef1dbd62e9
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-02-24 21:07:45 +0100

    Added THEME label

--- publishconf.py
+++ publishconf.py
@@ -18,6 +18,9 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = False
 
+# Theme
+# THEME = '../themes/bold'
+
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
