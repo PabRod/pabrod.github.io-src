646ccbe3dcddea21c758c28315aa955898881ac8
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-04-16 20:11:14 +0200

    Added static files

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,6 +13,8 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'English'
 
+STATIC_PATHS = ['images', 'pdfs']
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
